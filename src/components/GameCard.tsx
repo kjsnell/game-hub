8c2cd5bf71f13b41ccc6f6229cca7e89b72be177
Karen Snell <karenjsnell@gmail.com>
2024-01-21 16:49:24 -0700

    Refactored to remove duplicated card styles.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,12 +5,12 @@ import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/imageUrl';
 
 interface Props {
- game: Game;
+    game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width='300px' borderRadius={10} overflow='hidden'>
+    <Card>
         <Image src={getCroppedImageUrl(game.background_image)}/>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
