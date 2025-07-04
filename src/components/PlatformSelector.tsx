83f8c77adc90c34f162019c038decefe27ee8613
Karen Snell <karenjsnell@gmail.com>
2025-07-04 17:41:29 -0600

    Updated platforms to call API for data.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,26 +1,35 @@
-import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react'
-import React from 'react'
-import { BsChevronDown } from 'react-icons/bs'
-import usePlatforms from '../hooks/usePlatforms'
-import { Platform } from '../hooks/useGames'
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
 interface Props {
-    onSelectPlatform: (platform: Platform) => void
-    selectedPlatform: Platform  | null;
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform}: Props) => {
-    const {data, error} = usePlatforms();
-  
-    if (error) return null;
-    return (
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}> { selectedPlatform?.name || 'Platform'} </MenuButton>
-        <MenuList>
-            {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
-        </MenuList>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {" "}
+        {selectedPlatform?.name || "Platform"}{" "}
+      </MenuButton>
+      <MenuList>
+        {data?.results.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
-  )
-}
+  );
+};
 
-export default PlatformSelector
+export default PlatformSelector;
